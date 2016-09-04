d9bf75623652c42a736763f18ecbfc58ae16304c
Abby Vermaas <abbyvermaas@hotmail.com>
2016-09-04 15:55:22 +0200

    add dummy test

---
+++ solution/Calculator.Tests/TestCalculator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Calculo.Calculator.Tests
+{
+    [TestClass]
+    public class TestCalculator
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.IsTrue(Calculator.Library.Calculator.Test(true));
+        }
+    }
+}
