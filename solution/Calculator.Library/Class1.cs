222037d5e41775101890ec90eccc8e66d3f92d29
Abby Vermaas <abbyvermaas@hotmail.com>
2016-09-04 12:18:42 +0200

    add class library

---
+++ solution/Calculator.Library/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculo.Calculator.Library
+{
+    public class Class1
+    {
+    }
+}
