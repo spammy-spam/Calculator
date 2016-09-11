831c413c180a49519ebe6634a047f90d181d6726
Abby Vermaas <abbyvermaas@hotmail.com>
2016-09-11 12:08:17 +0200

    view

---
+++ Website/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Calculo.Calculator.Website.Models;
+
+namespace Calculo.Calculator.Website.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Index(string sum)
+        {
+            var calculation = new Calculation();
+
+            calculation.Sum = sum;
+            calculation.Answer = Calculator.Library.Calculator.Calculate(sum);
+
+
+            return View(calculation);
+        }
+    }
+}
